d236424cf7c97b6e4f2630385babf91d96e502b9
nitaking <10850034+nitaking@users.noreply.github.com>
2018-06-15 01:42:36 +0900

    :tada: init

---
+++ App.vue
@@ -0,0 +1,21 @@
+import todo-input from './src/components/TodoInput'
+
+<template>
+  <view class="container">
+    <text class="welcome">Vue Nativeへようこそ</text>
+  </view>
+</template>
+ 
+<style>
+.container {
+  background-color: #333;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.welcome {
+  color: white;
+  font-size: 20px;
+  margin: 20px;
+}
+</style>
